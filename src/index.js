ad202e6581735bc5ee55e9e23eefd3b78a57485b
nekalave <nekalave@gmail.com>
2024-06-12 15:02:27 +0300

    pr resolved

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { fetchBreeds, fetchImg } from './cat-api';
 const divCat = document.querySelector('.cat-info');
 const loader = document.querySelector('.loader');
 
+
 let select = new SlimSelect({
   select: '#selectElement',
   settings: {
@@ -13,11 +14,10 @@ let select = new SlimSelect({
   },
   events: {
     afterClose: () => {
-      let selectedCat = select.getSelected();
-      let localShit = [];
+      let selectValue = select.getSelected();
       loader.style.display = 'inline-block';
       select.settings.disabled = true;
-      fetchImg(selectedCat, localShit, loader, select, divCat)
+      renderCatInfo(selectValue)
     },
     beforeClose: () => {
       while (divCat.firstChild) {
@@ -27,4 +27,32 @@ let select = new SlimSelect({
   },
 });
 
-fetchBreeds(loader, select)
+const renderCatInfo = async (selectValue) => {
+  loader.style.display = 'block';
+ const selectedCatData = await fetchImg(selectValue)
+  const arr = selectedCatData.map(d => `
+      <img alt='${d.breeds[0].name}' src='${d.url}' width='400'/>
+      <div style='margin-left: 20px'>
+        <h2>${d.breeds[0].name}</h2>
+        <div style='width: 600px; margin-bottom: 20px'>${d.breeds[0].description}</div>
+        <div style='width: 600px'><span style='font-weight: bold'>Temperament: </span>${d.breeds[0].temperament}</div>
+      </div>
+    `).join('');
+  divCat.insertAdjacentHTML('beforeend', arr);
+  loader.style.display = 'none';
+  select.settings.disabled = false;
+}
+
+const fillSelectFunc = async () => {
+  loader.style.display = 'block';
+  let storedCats = await fetchBreeds()
+  const mappedStore = storedCats.map(cat => ({
+    text: cat.name,
+    value: cat.reference_image_id,
+  }));
+  select.setData(mappedStore);
+  loader.style.display = 'none';
+  select.settings.disabled = false;
+}
+
+fillSelectFunc()
